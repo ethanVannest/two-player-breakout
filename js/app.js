e260e737cfbab9d338f9ae6c73bb44fe86df7411
ethanvannest <ethan3vannest@gmail.com>
2022-07-16 10:15:46 -0600

    have ball moving

--- js/app.js
+++ js/app.js
@@ -1,4 +1,49 @@
 console.log('this works')
+const startGame = () => {
+
+}
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-80;
+let dx = 2;
+let dy = -2;
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
+// ctx.beginPath();
+// ctx.arc(50, 50, 10, 0, Math.PI*2);
+// ctx.fillStyle = "#0095DD";
+// ctx.fill();
+// ctx.closePath();
+// let x = canvas.width/2;
+// let y = canvas.height-30;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+
+
 //DEFINE function called start screen
     // change screen to actual game
 //DEFINE function called start game
