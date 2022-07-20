01e990776e58ccd0445d7d6395ae3d0a268e4d71
ethanvannest <ethan3vannest@gmail.com>
2022-07-19 19:53:24 -0600

    added movement

--- js/app.js
+++ js/app.js
@@ -55,9 +55,13 @@ class Game{
 const game = new Game
 class Paddle{
     constructor(){
+        this.rightPressed = false
+        this.leftPressed = false
         this.paddleHeight = 20
         this.paddleWidth = 120
         this.paddleX = (game.canvas.width-this.paddleWidth)/2
+        this.moveRight = false
+        this.moveLeft = false
     }
 }
 const paddle = new Paddle
@@ -106,39 +110,61 @@ function draw() {
     if(game.canvasY + game.drawnY > game.canvas.height-game.ballRadius || game.canvasY + game.drawnY < game.ballRadius) {
         game.drawnY = -game.drawnY;
         //if the ball is touching the top or bottom of the canvas then it is reversed
+        }
     }
-    
-}
-
-
-//DEFINE function called start screen
+    function  keydown(e){
+        if (e.code === 'ArrowRight'){
+            // console.log(paddle.rightPressed)
+            rightPressed = true;
+        } else if (e.code === 'ArrowLeft' ) {
+            leftPressed = true;
+        }
+    }
+    console.log(paddle.rightPressed)
+    function  keyup(e){
+        if (e.code === 'ArrowRight'){
+            paddle.rightPressed = false;
+            console.log(e.code)
+        } else if (e.code === 'ArrowLeft' ) {
+            console.log(e.code)
+            paddle.leftPressed = false;
+        }
+    }
+    if (paddle.rightPressed){
+        paddle.paddleX += 7
+    } else if (paddle.leftPressed){
+        paddle.paddleX -= 7
+    }
+    document.addEventListener("keydown", keydown) 
+    document.addEventListener('keyup', keyup)
+    //DEFINE function called start screen
     // change screen to actual game
-//DEFINE function called start game
-//WHEN clicked start game
-//ASK for player one name
+    //DEFINE function called start game
+    //WHEN clicked start game
+    //ASK for player one name
     //IF empty name player one
     //END if 
-//ASK (after player one) as for player two name
+    //ASK (after player one) as for player two name
     //IF name is empty use name 'Player two'
-//maybe* add a count down from three to actually begin the game
-//End Start game
-//SET intereval for every second that passes add 5 Points to whoever is playing at the time
-//everytime that a box is touched/broken add 10 points to whoever is up at the time
-//IF time reaches 40 seconds  
+    //maybe* add a count down from three to actually begin the game
+    //End Start game
+    //SET intereval for every second that passes add 5 Points to whoever is playing at the time
+    //everytime that a box is touched/broken add 10 points to whoever is up at the time
+    //IF time reaches 40 seconds  
     //THEN display message that says times up...Switching to player two
     //RESET blocks
     //RESET timer 
     //change score to player two but KEEP player one on the screen
-//END IF
-
-//MAKE mouse move along within a certain box on the page
-//WITHIN that box move a paddle only horizontal following mouse
-//ON game start 
-//START WITH ball on the paddle then send it out straight
-//ADD ball collision
+    //END IF
+    
+    //MAKE mouse move along within a certain box on the page
+    //WITHIN that box move a paddle only horizontal following mouse
+    //ON game start 
+    //START WITH ball on the paddle then send it out straight
+    //ADD ball collision
     //walls do not break
     //bricks break and dissapear for 3 seconds and come back in the same place they dissapeared from
-//AFTER PLAYER TWOs 40 seconds are up 
+    //AFTER PLAYER TWOs 40 seconds are up 
     //BLUR game
     //bring up a .6 opacity end game screen that shows who won and shows two buttons
         //BUTTON ONE is go home which takes you to the start game screen
